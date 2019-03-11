2614fe3fccc84dc05d884c7dacb39a99ae749643
estagio2018 <47534040+estagio2018@users.noreply.github.com>
2019-03-11 13:46:57 -0300

    Update and rename HC_05_Test_Mode.ino to HC_05_Seting_Mode.ino

--- HC_05_Seting_Mode.ino
+++ HC_05_Seting_Mode.ino
@@ -1,16 +1,11 @@
-//Include the SoftwareSerial library
-#include "SoftwareSerial.h"
-
-//Create a new software  serial
-SoftwareSerial bluetooth(2, 3); //TX, RX (Bluetooth)
+#include "SoftwareSerial.h"  //Include the SoftwareSerial library
+SoftwareSerial bluetooth(2, 3);  //TX, RX pins from Bluetooth
+                                 //Create a new software serial
 
 void setup() {
-  //Initialize the hardware serial
   Serial.begin(9600);
   Serial.println(F("Type the AT commands:"));
-
-  //Initialize the software serial
-  bluetooth.begin(38400);
+  bluetooth.begin(9600);  //Initialize the software serial
 }
 
 void loop() {
@@ -18,8 +13,9 @@ void loop() {
   if (Serial.available()) {
     char ATCommand = Serial.read(); //Read and save the byte
     bluetooth.write(ATCommand);  //Send the byte to bluetooth by software serial
-    Serial.print(ATCommand);  //Echo
+    Serial.print(ATCommand);
   }
+  
   //Check received a byte from bluetooth by software serial
   if (bluetooth.available()) {
     while (bluetooth.available()){
